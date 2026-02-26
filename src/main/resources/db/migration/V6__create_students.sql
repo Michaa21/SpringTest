c3d1d9f60c4718abe356c433200de3cf7ea43c67
Mikhail Lukin <21lukin21@gmail.com>
2026-02-26 13:05:28 +0500

    оставлены миграции users и profiles, остальные вынесены в отдельные PR

--- src/main/resources/db/migration/V6__create_students.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE spring_test.students
-(
-    id BIGSERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL
-);
--- src/main/resources/db/migration/V7__create_lessons.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE spring_test.lessons
-(
-    id BIGSERIAL PRIMARY KEY,
-    title VARCHAR(100) NOT NULL
-);
