860623bdfc31b6b75f6e22af3bc2526e57b357ea
Mikhail Lukin <21lukin21@gmail.com>
2026-03-19 09:09:06 +0500

    chore: update generated OpenAPI code

---
+++ src/main/java/com/example/springtest/api/dto/request/StudentCreateRequest.java
@@ -0,0 +1,134 @@
+package com.example.springtest.api.dto.request;
+
+import com.example.springtest.api.dto.request.LessonCreateRequest;
+import com.example.springtest.api.dto.request.StudentCreateRequest;
+
+
+import java.net.URI;
+import java.util.Objects;
+import com.example.springtest.api.dto.request.LessonCreateRequest;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.openapitools.jackson.nullable.JsonNullable;
+import java.time.OffsetDateTime;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+
+import java.util.*;
+import jakarta.annotation.Generated;
+
+/**
+ * StudentCreateRequest
+ */
+
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2026-03-18T13:04:20.490251100+05:00[Asia/Yekaterinburg]")
+public class StudentCreateRequest {
+
+  private String name;
+
+  @Valid
+  private List<@Valid LessonCreateRequest> lessons;
+
+  public StudentCreateRequest() {
+    super();
+  }
+
+  /**
+   * Constructor with only required parameters
+   */
+  public StudentCreateRequest(String name) {
+    this.name = name;
+  }
+
+  public StudentCreateRequest name(String name) {
+    this.name = name;
+    return this;
+  }
+
+  /**
+   * Get name
+   * @return name
+  */
+  @NotNull @Size(min = 1, max = 100) 
+  @Schema(name = "name", requiredMode = Schema.RequiredMode.REQUIRED)
+  @JsonProperty("name")
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public StudentCreateRequest lessons(List<@Valid LessonCreateRequest> lessons) {
+    this.lessons = lessons;
+    return this;
+  }
+
+  public StudentCreateRequest addLessonsItem(LessonCreateRequest lessonsItem) {
+    if (this.lessons == null) {
+      this.lessons = new ArrayList<>();
+    }
+    this.lessons.add(lessonsItem);
+    return this;
+  }
+
+  /**
+   * Get lessons
+   * @return lessons
+  */
+  @Valid 
+  @Schema(name = "lessons", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+  @JsonProperty("lessons")
+  public List<@Valid LessonCreateRequest> getLessons() {
+    return lessons;
+  }
+
+  public void setLessons(List<@Valid LessonCreateRequest> lessons) {
+    this.lessons = lessons;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    StudentCreateRequest studentCreateRequest = (StudentCreateRequest) o;
+    return Objects.equals(this.name, studentCreateRequest.name) &&
+        Objects.equals(this.lessons, studentCreateRequest.lessons);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(name, lessons);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class StudentCreateRequest {\n");
+    sb.append("    name: ").append(toIndentedString(name)).append("\n");
+    sb.append("    lessons: ").append(toIndentedString(lessons)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
---
+++ src/main/java/com/example/springtest/api/dto/response/ProfileResponse.java
@@ -0,0 +1,109 @@
+package com.example.springtest.api.dto.response;
+
+import com.example.springtest.api.dto.response.ProfileResponse;
+
+
+import java.net.URI;
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import org.openapitools.jackson.nullable.JsonNullable;
+import java.time.OffsetDateTime;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+
+import java.util.*;
+import jakarta.annotation.Generated;
+
+/**
+ * ProfileResponse
+ */
+
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2026-03-18T13:04:20.800585400+05:00[Asia/Yekaterinburg]")
+public class ProfileResponse {
+
+  private String firstName;
+
+  private String lastName;
+
+  public ProfileResponse firstName(String firstName) {
+    this.firstName = firstName;
+    return this;
+  }
+
+  /**
+   * Get firstName
+   * @return firstName
+  */
+  
+  @Schema(name = "firstName", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+  @JsonProperty("firstName")
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public ProfileResponse lastName(String lastName) {
+    this.lastName = lastName;
+    return this;
+  }
+
+  /**
+   * Get lastName
+   * @return lastName
+  */
+  
+  @Schema(name = "lastName", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+  @JsonProperty("lastName")
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    ProfileResponse profileResponse = (ProfileResponse) o;
+    return Objects.equals(this.firstName, profileResponse.firstName) &&
+        Objects.equals(this.lastName, profileResponse.lastName);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(firstName, lastName);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class ProfileResponse {\n");
+    sb.append("    firstName: ").append(toIndentedString(firstName)).append("\n");
+    sb.append("    lastName: ").append(toIndentedString(lastName)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
